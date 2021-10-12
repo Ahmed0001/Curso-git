ddb7d9b0b66ca9b6173c54ba9e3dbb2e4dd598bc
Ahmed <ahmedhpkd@gmail.com>
2021-10-11 22:45:37 -0300

    Testando escrita de descrição

---
+++ While/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int num = 0;
+                   
+
+            while (num != 2022) { 
+
+                Console.WriteLine("Digite sua senha");
+                num = int.Parse(Console.ReadLine());
+                string result = num != 2022 ? "Senha Inválida !" : "Acesso permitido";
+
+                Console.WriteLine(result);
+
+            }
+            
+            int codigo = 0;
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            
+            while (codigo != 4)
+            {
+                Console.WriteLine("Digita um cod");
+                codigo = int.Parse(Console.ReadLine());
+
+                switch (codigo)
+                {
+                    case 1:
+                        ++alcool;
+                        break;
+                    case 2:
+                        ++gasolina;
+                        break;
+                    case 3:
+                        ++diesel;
+                        break;
+                    default:
+                        Console.WriteLine("Muito Obrigado !");
+                        break;
+                }
+                Console.WriteLine($"Alcool:{alcool}\nGasolina:{gasolina}\nDiesel:{diesel}");
+            }
+            */
+
+        }
+
+
+    }
+}
